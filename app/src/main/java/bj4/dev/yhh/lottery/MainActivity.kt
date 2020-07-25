2a1c6dc9461aa8325a1602edbc015ad0e17697c7
yen <s011208@gmail.com>
2020-07-26 00:22:26 +0200

    First commit

---
+++ app/src/main/java/bj4/dev/yhh/lottery/MainActivity.kt
@@ -0,0 +1,11 @@
+package bj4.dev.yhh.lottery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
