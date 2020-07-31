80a3e96b58869f66cf79f570e13bed725f296896
yen <s011208@gmail.com>
2020-07-31 19:56:13 +0200

    add livedata async

--- app/src/main/java/bj4/dev/yhh/lottery/table/large/LargeTableViewModel.kt
+++ app/src/main/java/bj4/dev/yhh/lottery/table/large/LargeTableViewModel.kt
@@ -1,23 +1,31 @@
 package bj4.dev.yhh.lottery.table.large
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import bj4.dev.yhh.lottery.util.LiveDataMapAsync
 import bj4.dev.yhh.lotterydata.LotteryType
-import bj4.dev.yhh.lotterydata.local.dao.LotteryEntity
 import bj4.dev.yhh.lotterydata.repository.LotteryRepository
 
 class LargeTableViewModel(private val repository: LotteryRepository) : ViewModel() {
 
-    val mutableLotteryCategory: MutableLiveData<LotteryType> = MutableLiveData()
+    private val disposedList = ArrayList<LiveDataMapAsync<*, *>>()
+
+    private val mutableLotteryCategory: MutableLiveData<LotteryType> = MutableLiveData()
 
     fun searchByCategory(param: LotteryType) {
         mutableLotteryCategory.value = param
     }
 
-    val lotteryObservable: LiveData<List<LotteryEntity>> =
-        Transformations.switchMap(mutableLotteryCategory) { param ->
+    val lotteryObservable =
+        LiveDataMapAsync(Transformations.switchMap(mutableLotteryCategory) { param ->
             repository.getFromLocalLiveData(param)
-        }
+        }) {
+            return@LiveDataMapAsync this
+        }.also { disposedList.add(it) }.mapAsync()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposedList.forEach { it.dispose() }
+    }
 }
--- app/src/main/java/bj4/dev/yhh/lottery/util/Utilities.kt
+++ app/src/main/java/bj4/dev/yhh/lottery/util/Utilities.kt
@@ -1,8 +1,17 @@
 package bj4.dev.yhh.lottery.util
 
 import android.app.Activity
+import androidx.annotation.MainThread
+import androidx.arch.core.util.Function
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 object UiUtilities {
 
@@ -12,4 +21,25 @@ object UiUtilities {
     }
 
     fun makeDateFormat() = SimpleDateFormat("MM / dd / yyyy", Locale.getDefault())
+}
+
+class LiveDataMapAsync<X, Y>(private val source: LiveData<Y>, private val onChange: Y.() -> X) :
+    MediatorLiveData<X>() {
+    private val compositeDisposable = CompositeDisposable()
+
+    fun mapAsync(): LiveData<X> {
+        return MediatorLiveData<X>().apply {
+            addSource(source) {
+                compositeDisposable.add(Observable.fromCallable {
+                    postValue(onChange(it))
+                }.subscribeOn(Schedulers.io()).subscribe())
+            }
+        }
+    }
+
+    fun dispose() {
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+    }
 }
